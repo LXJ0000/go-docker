e1a320c360b79a83f83476a4dee205cbc1e530eb
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-09-01 23:47:09 +0800

    feat: 实现进程隔离

--- main.go
+++ main.go
@@ -35,10 +35,16 @@ func run() {
 func child() {
 	cmd := exec.Command(os.Args[2]) // 避免对主进程的 hostname 进行修改
 	syscall.Sethostname([]byte("container"))
+	// MS_NOEXEC 本文件系统不允许运行其他程序
+	// MS_NOSUID 本文件系统运行其他程序的时候，不允许set-user-id和set-group-id
+	// MS_NODEV 从Linux2.4以来，所有mount的系统都会默认设定的参数
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "") // TODO 隔离文件系统
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		panic(err)
 	}
+	syscall.Unmount("/proc", 0)
 }
